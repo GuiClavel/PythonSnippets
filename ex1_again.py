677c3a68a64d72a4eb9ac4375b3dc18b7802e77b
GuiClavel <53654675+GuiClavel@users.noreply.github.com>
2023-01-29 21:17:30 -0400

    Add files via upload

---
+++ ex1_again.py
@@ -0,0 +1,11 @@
+ 
+import requests
+from bs4 import BeautifulSoup
+ 
+base_url = 'http://www.nytimes.com'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, features="html.parser")
+
+open_file = open('myFileTest03.txt', 'w')
+open_file.write('test of text')   
+open_file.close()  
